4c3ef415fdddee4a6957e6b2b6ea93506f9409de
Nicovz21 <61903751+Nicovz21@users.noreply.github.com>
2021-03-10 12:56:07 -0500

    Chapter 8 push attempt 4

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
 
+
         intent.putExtra(Intent.EXTRA_TEXT, text);
         shareActionProvider.setShareIntent(intent);
     }
